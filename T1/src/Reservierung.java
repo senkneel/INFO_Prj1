f2d2a927e291e4bcf0fc387df8bb38bc4113201d
ypK <102804833+senkneel@users.noreply.github.com>
2022-06-19 22:29:30 +0200

    Add files via upload

---
+++ T1/src/Reservierung.java
@@ -0,0 +1,19 @@
+
+public class Reservierung {
+
+	public int bnr;
+	public int start;
+	public int end;
+	public int pers;
+	public int knr;
+	
+	public Reservierung(int bnr, int start, int end, int pers, int knr) {
+		super();
+		this.bnr = bnr;
+		this.start = start;
+		this.end = end;
+		this.pers = pers;
+		this.knr = knr;
+	}
+	
+}
